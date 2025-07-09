8e798d57ebc37a2617bcd1bd25961e1def774c39
JoseHenao1 <jose.henao1@udea.edu.co>
2025-07-09 15:59:06 -0500

    feat: agregar monitoreo de métricas del sistema y dashboard en Grafana para procesos, hilos, tiempo de actividad y tráfico de red

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from prometheus_client import start_http_server
-from app.routes import metrics, profiling
-from app.services import cpu_monitor, memory_monitor, io_monitor
+from app.routes import metrics, profiling, system
+from app.services import cpu_monitor, memory_monitor, io_monitor, system_monitor
 import threading
 import time
 
@@ -9,6 +9,7 @@ app = FastAPI(title="Performance API")
 
 app.include_router(metrics.router)
 app.include_router(profiling.router)
+app.include_router(system.router)
 
 # Iniciar Prometheus exporter
 start_http_server(8001)
@@ -19,6 +20,7 @@ def actualizar_metricas():
         cpu_monitor.get_cpu_percent()
         memory_monitor.get_memory_usage()
         io_monitor.get_disk_usage()
+        system_monitor.update_system_metrics() 
         time.sleep(5)
 
 threading.Thread(target=actualizar_metricas, daemon=True).start()
